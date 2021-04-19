e015ce40b0125fbe85c4a4eb3abaf4f1a58713a4
Osamudiame  Akioya <jonathanakioya@gmail.com>
2021-04-19 18:24:57 +0100

    write the fetched data to a posts.txt file inside a result folder

--- fetch.js
+++ fetch.js
@@ -1,6 +1,28 @@
-const fs = require('fs')
+const fs = require('fs');
+const htt = require('https')
 const fetch = require('node-fetch')
 
-let fetchData = fetch('http://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((data) => console.log(data))
+// Use the fetch method to get JSON data from the API
+
+fetch('http://jsonplaceholder.typicode.com/posts')
+// Write the JSON data gotten from the API to a file called posts.txt in a folder named result
+  .then(res => res.json())
+  //stringify the fetched JSON data
+  //write the fetched data to a file named posts.txt in a folder named result
+  .then(json => {
+    fs.writeFile('./result/posts.txt', JSON.stringify(json), () => {
+      
+        console.log('new file writen successfully')
+   
+   
+    })
+
+  
+ 
+  })
+
+
+
+
+
+ 
